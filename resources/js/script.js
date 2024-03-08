79970daedba436eac397a6984cf71f206c378bb1
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-08 13:54:03 +0100

    created the code for create div inside at wrapper

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,27 @@
 
 let boxNumber = 100
 let boxWrapper = document.createElement("div")
-boxWrapper.className = "container-lg d-flex align-items"
+boxWrapper.className = "d-flex justify-content-center align-content-center align-items-center flex-wrap mx-auto my-5"
+boxWrapper.setAttribute("id", "wrapper");
+let tmpHTML = "";
+let typeOfBox = "";
 
 for (let i = 1; i < 101; i++) {
 
     if ((i % 3 === 0) && (i % 5 === 0)) { //caso più stringente
-        console.log("fizzbuzz");
+        typeOfBox = "fizzbuzz";
     } else if (i % 3 === 0) {
-        console.log("fizz");
+        typeOfBox = "fizz";
     } else if (i % 5 === 0) {
-        console.log("buzz");
+        typeOfBox = "buzz";
     } else {
-        console.log(i);
-    }
+        typeOfBox = i;
+    };
+    tmpHTML += `<div class="box d-flex align-items-center justify-content-center rounded-1 ${isNaN(typeOfBox) ? typeOfBox : ""}">${typeOfBox}</div>`;
     // i % 3 === 0 && i % 5 === 0 ? console.log("fizzbuzz") : i % 3 === 0 ? console.log("fizz") : i % 5 === 0 ? console.log("buzz") : console.log(i);
 }
+
+boxWrapper.innerHTML = tmpHTML
+document.getElementById("app").append(boxWrapper)
 
 
